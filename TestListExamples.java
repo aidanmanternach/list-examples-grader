8b034188c85f81d3de2515457e97b7f4de16bcbb
aidanmanternach <92654166+aidanmanternach@users.noreply.github.com>
2022-11-10 14:01:11 -0800

    grader bash script

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,47 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import java.util.ArrayList;
+
 public class TestListExamples {
   // Write your grading tests here!
+  class CheckerContainsB implements StringChecker {
+    public boolean checkString(String s) {
+      return s.contains("b");
+    }
+  }
+
+  @Test
+  public void testFilter() {
+    StringChecker sc = new CheckerContainsB();
+    ArrayList<String> arr = new ArrayList<>();
+    arr.add("ball");
+    arr.add("hi");
+    arr.add("bad");
+    ArrayList<String> expectedArr = new ArrayList<>();
+    expectedArr.add("ball");
+    expectedArr.add("bad");
+    assertEquals(expectedArr, ListExamples.filter(arr, sc));
+  }
+  
+  @Test
+  public void testMerge() {
+    ArrayList<String> arr1 = new ArrayList<>();
+    arr1.add("a");
+    arr1.add("b");
+    arr1.add("c");
+    ArrayList<String> arr2 = new ArrayList<>();
+    arr2.add("a");
+    arr2.add("d");
+    arr2.add("f");
+    ArrayList<String> output = new ArrayList<>();
+    output.add("a");
+    output.add("a");
+    output.add("b");
+    output.add("c");
+    output.add("d");
+    output.add("f");
+
+    assertEquals(output, ListExamples.merge(arr1, arr2));
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,41 @@
 # Create your grading script here
 
-set -e
+# set -e
 
 rm -rf student-submission
 git clone $1 student-submission
 
+cp TestListExamples.java student-submission/
+cp -r lib student-submission/
+cd student-submission
+
+if [[ -f ListExamples.java ]]
+then
+  javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java 2> compile-error.txt
+
+  if [[ $? -eq 0 ]]
+  then
+    java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > runtime-error.txt
+
+    ERR=$(grep "FAILURES" runtime-error.txt)
+    if [[ $ERR == "FAILURES!!!" ]]
+    then
+      cat runtime-error.txt
+      echo "Code failed one or more of the tests"
+      echo "Your Grade: Fail"
+      echo "Try again"
+      exit
+    fi
+
+    echo "Your Grade: Passed"
+    echo "Good Job"
+    exit
+  fi
+
+  echo "Compile Error"
+  cat compile-error.txt
+  exit
+fi
+
+echo "The file ListExamples.java was not found."
+exit
